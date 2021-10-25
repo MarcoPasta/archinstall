4076595fd54fb045b609a4cb36f66fa5227bfd9c
MarcoPasta <lucachiarelli@hotmail.de>
2021-10-25 21:27:57 +0200

    added a few start structures

--- archinstall.sh
+++ archinstall.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+# Variables for installation
+PASS=""
+
 echo "creating partition";
-sleep 2;
 fdisk /dev/sda << EOF
 o
 n
@@ -12,23 +14,24 @@ a
 
 w
 EOF
-sleep 4;
+
+
 echo "creating ext4 format";
 mkfs.ext4 /dev/sda1;
-sleep 4;
+
 echo "mounting system";
 mount /dev/sda1 /mnt;
+
 echo "mounting successful";
-sleep 4;
+
 echo "calling pacstrap to install base system";
 pacstrap /mnt base base-devel linux linux-firmware dhcpcd nano vim networkmanager iwd << EOF
 y
 EOF
-sleep 2;
+
 echo "generating fstab file";
-sleep 1;
 genfstab -U /mnt > /mnt/etc/fstab;
-sleep 1;
+
 echo "chrooting into new system";
 arch-chroot /mnt << "EOT"
 echo arch > /etc/hostname;
@@ -38,12 +41,27 @@ locale-gen;
 echo KEYMAP=de-latin1 > /etc/vconsole.conf;
 echo FONT=lat9w-16 >> /etc/vconsole.conf;
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime;
+passwd << EOF
+$PASS
+$PASS
+EOF
+useradd -mG wheel -s /bin/bash luca
+passwd luca << EOF
+$PASS
+$PASS
+EOF
 mkinitcpio -p linux;
 pacman -S --noconfirm grub;
 grub-install --recheck /dev/sda;
 grub-mkconfig -o /boot/grub/grub.cfg;
 echo '%wheel ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers;
+pacman -S cinnamon gdm gedit gimp alacritty git wget vim okular vlc vivaldi geeqie flameshot nm-applet
+systemctl enable gdm
+systemctl enable NetworkManager
+
 EOT
+
+# nearly done
 umount /mnt
 echo "ready for reboot?"
 sleep 4;
