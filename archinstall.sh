512c7ad89586c97dd494d1b2db3f843c666feded
MarcoPasta <lucachiarelli@hotmail.de>
2021-10-25 20:31:58 +0200

    update 2

--- archinstall.sh
+++ archinstall.sh
@@ -20,7 +20,7 @@ echo "mounting system";
 mount /dev/sda1 /mnt;
 echo "mounting successful";
 sleep 4;
-echo "calling pacstrap to install base system\";
+echo "calling pacstrap to install base system";
 pacstrap /mnt base base-devel linux linux-firmware dhcpcd nano vim networkmanager iwd << EOF
 y
 EOF
@@ -31,31 +31,20 @@ genfstab -U /mnt > /mnt/etc/fstab;
 sleep 1;
 echo "chrooting into new system";
 arch-chroot /mnt << "EOT"
-sleep 1;
 echo arch > /etc/hostname;
-sleep 1;
 echo LANG=de_DE.UTF-8 > /etc/locale.conf;
-sleep 1;
 sed --in-place=.bak 's/^#de_DE\.UTF-8/de_DE\.UTF-8/' /etc/locale.gen;
-sleep 1;
 locale-gen;
-sleep 1;
 echo KEYMAP=de-latin1 > /etc/vconsole.conf;
-sleep 1;
 echo FONT=lat9w-16 >> /etc/vconsole.conf;
-sleep 1;
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime;
-sleep 1;
 mkinitcpio -p linux;
-sleep 1;
 pacman -S --noconfirm grub;
-sleep 1;
-grub-install;
-sleep 1;
+grub-install --recheck /dev/sda;
 grub-mkconfig -o /boot/grub/grub.cfg;
 echo '%wheel ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers;
 EOT
-unmount /mnt
+umount /mnt
 echo "ready for reboot?"
 sleep 4;
 reboot
