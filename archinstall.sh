7da1f4b87322e9a41cd7732499fb2045a3046709
MarcoPasta <lucachiarelli@hotmail.de>
2021-10-25 22:00:39 +0200

    keep trying

--- archinstall.sh
+++ archinstall.sh
@@ -41,15 +41,15 @@ locale-gen;
 echo KEYMAP=de-latin1 > /etc/vconsole.conf;
 echo FONT=lat9w-16 >> /etc/vconsole.conf;
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime;
-echo "root:$PASS" | chpasswd;
+passwd
 useradd -mG wheel -s /bin/bash luca;
-echo "luca:$PASS" | chpasswd;
+passwd luca
 mkinitcpio -p linux;
 pacman -S --noconfirm grub;
 grub-install --recheck /dev/sda;
 grub-mkconfig -o /boot/grub/grub.cfg;
 #echo '%wheel ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers;
-pacman -S --noconfirm cinnamon gdm gedit gimp alacritty git wget vim okular vlc vivaldi geeqie flameshot nm-applet;
+pacman -S --noconfirm cinnamon gdm gedit gimp alacritty git wget vim okular vlc vivaldi geeqie flameshot nm-applet
 systemctl enable gdm;
 systemctl enable NetworkManager;
 
