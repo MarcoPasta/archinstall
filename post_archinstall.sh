aea1e059f09ff422e07be77a2dbe694afcdcc614
MarcoPasta <lucachiarelli@hotmail.de>
2021-10-26 20:15:19 +0200

    split script in 2 files to avoid to many stdin streams

---
+++ post_archinstall.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#get password from the other file 
+PASS="$1";
+echo $PASS;
+sleep 4;
+
+# create locales
+echo arch > /etc/hostname;
+echo LANG=de_DE.UTF-8 > /etc/locale.conf;
+sed --in-place=.bak 's/^#de_DE\.UTF-8/de_DE\.UTF-8/' /etc/locale.gen;
+ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime;
+locale-gen;
+
+# set keyboard layout
+echo KEYMAP=de-latin1 > /etc/vconsole.conf;
+echo FONT=lat9w-16 >> /etc/vconsole.conf;
+
+# create root pw, user and user pw
+echo "root:$PASS" | chpasswd;
+useradd -mG wheel -s /bin/bash luca;
+echo "luca:$PASS" | chpasswd;
+
+# create init system / kernel & install grub 
+mkinitcpio -p linux;
+pacman -S --noconfirm grub;
+grub-install --recheck /dev/sda;
+grub-mkconfig -o /boot/grub/grub.cfg;
+
+# automatically put user into the sudoers file 
+#echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers;
+
+# install additional packages
+pacman -S --noconfirm cinnamon gdm gedit alacritty git wget vim okular vlc geeqie \
+flameshot network-manager-applet vivaldi vivaldi-ffmpeg-codecs bluez blueberry \
+chromium pcmanfm thunderbird libreoffice bitwarden xournalpp neofetch redshift\
+intel-ucode
+
+# enable system services on boot 
+systemctl enable gdm;
+systemctl enable NetworkManager;
+systemctl enable bluetooth;
---
+++ pre_archinstall.sh
@@ -0,0 +1,46 @@
+#!/bin/bashsh
+
+# Ideen für todo
+# fonts ändern, config .dotfiles kopieren
+
+#get input for password
+PASS="$1";
+echo $PASS;
+sleep 4; 
+
+
+# creating partitions with stdin stream
+fdisk /dev/sda << EOF
+o
+n
+p
+
+
+
+a
+
+w
+EOF
+
+
+# creating ext4 format and mount;
+mkfs.ext4 /dev/sda1;
+mount /dev/sda1 /mnt;
+
+
+# install base system;
+pacstrap /mnt base base-devel linux linux-firmware dhcpcd nano networkmanager iwd << EOF
+y
+EOF
+
+# creating fstab and chroot into new system
+genfstab -U /mnt > /mnt/etc/fstab;
+arch-chroot /mnt /bin/bash /root/archinstall/post_archinstall.sh $PASS
+
+# nearly done
+umount /mnt
+echo "ready for reboot?"
+sleep 4;
+reboot
+
+
